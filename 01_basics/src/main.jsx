113897f241df352dc25873f301f1a4da5aeeb66f
Basirul Islam Khan <basirul.k@appseconnect.com>
2024-10-14 18:22:16 +0530

    Understanding the React Flow and structure

--- 01_basics/src/main.jsx
+++ 01_basics/src/main.jsx
@@ -1,15 +1,42 @@
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+//import App from './App.jsx';
 
-// const root = createRoot(document.getElementById('root'));
+// function MyApp() {
+//   return (
+//     <div>
+//       <h1>Hello, world! Welcome to BK world!</h1>
+//     </div>
+//   );
+// }
 
-// root.render(<App />);
+//This will not work as we are not sending predefine parameteres
+// const ReactElement = {
+//   type: 'a',
+//   props: {
+//     href: 'https://www.google.com',
+//     target: '_blank',
+//   },
+//   children: 'Click here to visit Google',
+// };
+
+//This is will work as we are sending direct HTML
+
+// const ReactElement2 = (
+//   <a href="https://google.com" target="_blank">
+//     Click Here
+//   </a>
+// );
+
+const username = 'Basirul Khan';
+
+const ReactElement = React.createElement(
+  'a',
+  { href: 'https://google.com', target: '_blank' },
+  'Click here to visit Googles',
+  username
+);
 
 const root = createRoot(document.getElementById('root'));
 
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+root.render(ReactElement);
